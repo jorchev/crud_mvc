cac428f30a83b2689e6fe4cbb1b10c240781d51e
Jorge Vega <jorge.hvr@gmail.com>
2022-10-09 01:08:18 -0700

    Creacion de rutas

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+    define("CONTROLADOR_PRINCIPAL","Automoviles");
+    define("ACCION_PRINCIPAL","listado");
+
+?>
---
+++ core/routes.php
@@ -0,0 +1,40 @@
+<?php
+
+    function cargarControlador($controlador){
+
+        $archivoControlador = "controllers/".ucwords($controlador)."Controller.php";
+        $nombreControlador = ucwords($controlador)."Controller";
+
+        // Validar que el archivo exista
+        if(!is_file($archivoControlador)){
+            $archivoControlador = "controllers".CONTROLADOR_PRINCIPAL."Controller.php";
+        }
+
+        // Validar que la clase exista
+        if(!class_exists($nombreControlador)){
+            $nombreControlador = CONTROLADOR_PRINCIPAL."Controller";
+        }
+
+        require_once $archivoControlador;
+        $control = new $nombreControlador();
+
+        return $control;
+
+    }
+
+    function cargarAccion($control, $accion, $id = null){
+
+        if(isset($accion) && method_exists($control,$accion)){
+            if($id == null){
+                $control->$accion();
+            }else{
+                $control->$accion($id);
+            }
+        }else{
+            $accion = ACCION_PRINCIPAL;
+            $control->$accion();
+        }
+
+    }
+
+?>
