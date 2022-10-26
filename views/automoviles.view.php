ac8ec5cc57e1a60d669e7fac1fdcfef1ee4f5bf5
Jorge Vega <j.vega@mafasa.org>
2022-10-26 14:55:44 -0700

    Se implementa Bootstrap al proyecto

--- views/automoviles.view.php
+++ views/automoviles.view.php
@@ -5,50 +5,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $data['titulo']; ?></title>
+    <link rel="stylesheet" href="lib/css/bootstrap.min.css">
 </head>
 <body>
 
-    <h2><?php echo $data['titulo']; ?></h2>
+    <div class="container">
+        <h2><?php echo $data['titulo']; ?></h2>
+        <hr>
+        <a href="index.php?c=Automoviles&a=nuevo" class="btn btn-success mb-3">Agregar</a>
+        <table class="table table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>Placa</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Año</th>
+                    <th>Color</th>
+                    <th class="text-center">Opciones</th>
+                </tr>
+            </thead>
 
-    <a href="index.php?c=Automoviles&a=nuevo">Agregar</a>
-    <table border="1" width="80%">
-        <thead>
-            <tr>
-                <th>Placa</th>
-                <th>Marca</th>
-                <th>Modelo</th>
-                <th>Año</th>
-                <th>Color</th>
-                <th>Opciones</th>
-            </tr>
-        </thead>
+            <tbody>
 
-        <tbody>
+                <?php
 
-            <?php
+                    foreach($data['automoviles'] as $auto){
 
-                foreach($data['automoviles'] as $auto){
+                        echo "<tr>";
+                        echo "<td>{$auto['placa']}</td>";
+                        echo "<td>{$auto['marca']}</td>";
+                        echo "<td>{$auto['modelo']}</td>";
+                        echo "<td>{$auto['anio']}</td>";
+                        echo "<td>{$auto['color']}</td>";
+                        echo "<td class=\"text-center\">
+                                <a href=\"index.php?c=automoviles&a=modificar&id={$auto['id']}\" class=\"btn btn-sm btn-primary\">Modificar</a> 
+                                <a href=\"#\" onclick=\"confirmarEliminar('{$auto['placa']}',{$auto['id']})\" class=\"btn btn-sm btn-danger\">Eliminar</a>
+                            </td>";
+                        echo "</tr>";
 
-                    echo "<tr>";
-                    echo "<td>{$auto['placa']}</td>";
-                    echo "<td>{$auto['marca']}</td>";
-                    echo "<td>{$auto['modelo']}</td>";
-                    echo "<td>{$auto['anio']}</td>";
-                    echo "<td>{$auto['color']}</td>";
-                    echo "<td>
-                            <a href=\"index.php?c=automoviles&a=modificar&id={$auto['id']}\">Modificar</a> / 
-                            <a href=\"#\" onclick=confirmarEliminar('{$auto['placa']}',{$auto['id']})>Eliminar</a>
-                        </td>";
-                    echo "</tr>";
+                    }
 
-                }
+                ?>
 
-            ?>
+            </tbody>
 
-        </tbody>
+        </table>
+    </div>
 
-    </table>
-
+    <script src="lib/js/jquery-3.5.1.min.js"></script>
+    <script src="lib/js/bootstrap.bundle.min.js"></script>
     <script>
         function confirmarEliminar(placa,id){
 
