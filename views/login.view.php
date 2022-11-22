6da4b3aaf79129ae42e92d90f5ef875a148cd9ac
Jorge Vega <jorge.hvr@gmail.com>
2022-11-22 15:42:19 -0700

    Se realizaron ajustes al proyecto

--- views/login.view.php
+++ views/login.view.php
@@ -5,22 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $data['titulo']; ?></title>
+    <link rel="stylesheet" href="lib/css/bootstrap.min.css">
 </head>
 <body>
-
-    <h2><?php echo $data['titulo']; ?></h2>
-    <form action="index.php?c=Usuarios&a=iniciarSesion" method="post" id="iniciar-sesion" name="iniciar-sesion" autocomplete="off">
-        <div>
-            <label for="usuario">Usuario:</label>
-            <input type="text" name="usuario" id="usuario">
+    <div class="container col-12 col-md-6 col-lg-4 mt-5">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title"><?php echo $data['titulo']; ?></h2>
+                <form action="index.php?c=Usuarios&a=iniciarSesion" method="post" id="iniciar-sesion" name="iniciar-sesion" autocomplete="off">
+                    <div class="form-group">
+                        <label for="usuario" class="form-label mt-4">Usuario:</label>
+                        <input type="text" class="form-control" name="usuario" id="usuario">
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="form-label mt-4">Contraseña:</label>
+                        <input type="password" class="form-control" name="password" id="password">
+                    </div>
+                    <div class="text-end">
+                        <button type="submit" class="btn btn-primary mt-4" id="login" name="login">Iniciar sesión</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div>
-            <label for="password">Contraseña:</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <button type="submit" id="login" name="login">Iniciar sesión</button>
-        </div>
-    </form>
+    </div>
+    
+    <script src="lib/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
